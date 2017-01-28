3f2f8fc7c088eef0d988076ebb389ac2a423e66f
Paul-Andre Henegar <paul-andre.henegar@mail.mcgill.ca>
2017-01-28 13:20:31 -0500

    Created generateTuning header.

--- src/Tuning.h
+++ src/Tuning.h
@@ -1,7 +1,13 @@
+#include <vector>
+using namespace std;
+
 // both generators are in octaves.
 // gen1 is supposed to be the interval of equivalence.
 struct Tuning {
 	double gen1Size;
 	double gen2Size;
+	int noteNumber;
 }
+
+vector<double> generateMosScale(Tuning const &Tuning);
 
