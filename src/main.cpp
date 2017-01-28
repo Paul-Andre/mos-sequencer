b8051ff41e4d5b2460982dd5dbf00b66fa5d0477
Pozidriv <youri.tam@gmail.com>
2017-01-28 14:20:15 -0500

    Created audio callback function

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,16 @@
 #include <stdlib.h>
 
 #include <SDL2/SDL.h>
+#define MUS_PATH ""
+
+void audio_callback(void *userdata, Uint8 *stream, int len);
+
+static Uint8 *audio_pos;
+static Uint32 audio_len;
 
 int main(int argc, char **argv) {
 	bool quit = false;
 	SDL_Event e;
-	
 
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_Window *window = SDL_CreateWindow("Sequencer",
@@ -15,6 +20,32 @@ int main(int argc, char **argv) {
 						 640, 480,
 						 0);
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+
+	static Uint32 wav_length;
+	static Uint8 *wav_buffer;
+	static SDL_AudioSpec wav_spec;
+
+	if( SDL_LoadWAV(MUS_PATH, &wav_spec, &wav_length) == NULL )
+		return 1;
+
+	wav_spec.callback = my_audio_callback;
+	wav_spec.userdata = NULL;
+
+	audio_pos = wav_buffer;
+	audio_len = wav_length;
+
+	if( SDL_OpenAudio(&wav_spec, NULL) < 0 ){
+		fprintf(stderr, "Couldn't open audio: %s\n", SDL_GetError());
+		exit(-1);
+	}
+
+	SDL_PauseAudio(0);
+
+	while( audio_len > 0 )
+		SDL_Delay(100);
+
+	SDL_CloseAudio();
+	SDL_FreeWav(wav_buffer);
 
 	//drawScale(tuning, renderer);
 
@@ -32,3 +63,10 @@ void draw(SDL_Renderer* renderer) {
 	
 }
 
+void audio_callback(void *userdata, Uint8 *stream, int len){
+
+}
+
+void event_loop(){
+
+}
